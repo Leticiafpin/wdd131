466bfbab48feb7e19778db8085c297052add80ef
Leticia Figueiredo <pflele1@gmail.com>
2024-07-05 18:50:39 +0100

    final project

---
+++ week04/finalproject/script/final.js
@@ -0,0 +1,5 @@
+// Exemplo de código JavaScript (adapte conforme necessário)
+document.addEventListener('DOMContentLoaded', function () {
+    // Adicione seus scripts aqui
+    // Por exemplo, manipulação do DOM, eventos, etc.
+});
