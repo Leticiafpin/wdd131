0acbee0658d3e5be2f2b58dae67f82cfcba66d35
Leticia Figueiredo <pflele1@gmail.com>
2024-07-06 23:47:32 +0100

    ch

--- week04/script/fittemples.js
+++ week04/script/fittemples.js
@@ -129,29 +129,6 @@ const temples = [
 
 
 // Função para criar os "temple cards"
-function createTempleCards(templeArray) {
-    const container = document.getElementById("temple-cards-container");
-  
-    templeArray.forEach((temple) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
-  
-      const cardContent = `
-        <h3>${temple.name}</h3>
-        <p>Location: ${temple.location}</p>
-        <p>Date Dedicated: ${temple.dateDedicated}</p>
-        <p>Total Area: ${temple.areaSquareFeet} sq ft</p>
-        <img src="${temple.imageUrl}" alt="${temple.name}" loading="lazy">
-      `;
-  
-      card.innerHTML = cardContent;
-      container.appendChild(card);
-    });
-  }
-  
-  // Chame a função com o array de templos
-  createTempleCards(temples);
-
   function createTempleCards() {
     temples.forEach(temple => {
         let card = document.createElement("section");
@@ -162,6 +139,23 @@ function createTempleCards(templeArray) {
         let img = document.createElement("img");
 
         name.textContent = temple.templeName;
-        location.innerHTML = "<span class="label">Location:</span> ${temple.location}";
+        location.innerHTML = '<span class="label">Location:</span> ${temple.location}';
+        dedication.innerHTML = '<span class="label">Dedicated:</span> ${temple.dedicated}';
+        area.innerHTML = '<span class="label">Size:</span> ${temple.area}sq ft';
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", '${temple.templeName} Temple');
+        img.setAttribute("loading","lazy")
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+
+        document.querySelector(".res-grid").appendChild(card);
+
     })
   }
+  
+  // Chame a função com o array de templos
+  createTempleCards(temples);
